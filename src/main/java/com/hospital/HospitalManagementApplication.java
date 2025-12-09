ba12ff0c1d8cd7a8080aa72fc7ebfc7424817e43
Ironfist007 <cyaswanth.sonu@gmail.com>
2025-12-10 00:48:23 +0530

    Fix: Remove duplicate passwordEncoder bean definition

--- src/main/java/com/hospital/HospitalManagementApplication.java
+++ src/main/java/com/hospital/HospitalManagementApplication.java
@@ -3,10 +3,7 @@ package com.hospital;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Hospital Management System - Main Application Class
@@ -20,10 +17,5 @@ public class HospitalManagementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HospitalManagementApplication.class, args);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
